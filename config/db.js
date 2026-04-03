bea49347ecb356cc072df0d6aeb6968553aa755e
Leonardo Walter <leonardo.walterfb@gmail.com>
2026-04-02 23:57:29 -0300

    migração postgres

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
-const { Pool } = require("pg");
+const { Pool } = require("pg")
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
-  }
-});
+  },
+  family: 4 // 👈 AQUI SIM (correto)
+})
 
-module.exports = pool;
+module.exports = pool
