bc757bd4b8bb833f18dc8c0897b55142c5474b1f
Leonardo Walter <leonardo.walterfb@gmail.com>
2026-04-06 21:32:22 -0300

    ajuste de bugs

--- db.js
+++ db.js
@@ -4,7 +4,10 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
-  }
+  },
+  max: 5, // 🔥 limite de conexões (IMPORTANTE pro Neon)
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000
 })
 
 module.exports = pool
